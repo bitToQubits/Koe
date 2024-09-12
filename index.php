5e4ea275370dfdb06a372a040d4c1cfa0cd1e35c
Jorge Baez <22-0637@copphu.unphu.edu.do>
2024-09-12 05:04:12 -0400

    initial

---
+++ index.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <?php
+        include 'header.php';
+    ?>
+    <section class="h-screen">
+        <div class="relative top-1/4">
+            <h1 class="text-3xl text-center font-bold mb-4">
+                Produce voces realistas, hermosas y vibrantes <br> con nuestra plataforma
+            </h1>
+            <p class="text-center text-xl mb-8 font-light">En Koe investigamos el poder de la IA aplicada a la generación de audio.</p>
+            <div class="flex justify-center">
+                <button class="rounded-full text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Me interesa. Empecemos.</button>
+                <button class="rounded-full border border-gray-300 px-5 py-2.5 text-center me-2 mb-2 font-medium rounded-lg text-sm">Probar gratuitamente.</button>
+            </div>
+        </div>
+    </section>
+    <section class="mx-11 h-screen">
+        <div class="flex justify-center mb-5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="#1E52DC" class="bi bi-soundwave" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.5 2a.5.5 0 0 1 .5.5v11a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5m-2 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m4 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5m-6 1.5A.5.5 0 0 1 5 6v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m8 0a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m-10 1A.5.5 0 0 1 3 7v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5m12 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5"/>
+            </svg>
+        </div>
+        <p class="text-center text-xl font-light mb-5">Presentamos nuestro modelo generador TTS de vanguardia</p>
+        <h1 class="text-3xl text-center font-bold mb-10">Motor acústico</h1>
+        <form>
+            <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
+                <div class="flex items-center justify-between px-3 py-2 border-b">
+                    <div class="flex flex-wrap items-center divide-gray-200 sm:divide-x sm:rtl:divide-x-reverse">
+                        <div class="flex items-center space-x-1 rtl:space-x-reverse sm:pe-4">
+                            <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100">
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
+                                        <path d="M14.067 0H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.933-2ZM6.709 13.809a1 1 0 1 1-1.418 1.409l-2-2.013a1 1 0 0 1 0-1.412l2-2a1 1 0 0 1 1.414 1.414L5.412 12.5l1.297 1.309Zm6-.6-2 2.013a1 1 0 1 1-1.418-1.409l1.3-1.307-1.295-1.295a1 1 0 0 1 1.414-1.414l2 2a1 1 0 0 1-.001 1.408v.004Z"/>
+                                    </svg>
+                                    <span class="sr-only">Format code</span>
+                            </button>
+                            <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 0">
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM13.5 6a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm-7 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm3.5 9.5A5.5 5.5 0 0 1 4.6 11h10.81A5.5 5.5 0 0 1 10 15.5Z"/>
+                                    </svg>
+                                <span class="sr-only">Add emoji</span>
+                            </button>
+                        </div>
+                        <div class="flex flex-wrap items-center space-x-1 rtl:space-x-reverse sm:ps-4">
+                            <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 0">
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 18">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.5 3h9.563M9.5 9h9.563M9.5 15h9.563M1.5 13a2 2 0 1 1 3.321 1.5L1.5 17h5m-5-15 2-1v6m-2 0h4"/>
+                                    </svg>
+                                <span class="sr-only">Add list</span>
+                            </button>
+                            <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 0">
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M18 7.5h-.423l-.452-1.09.3-.3a1.5 1.5 0 0 0 0-2.121L16.01 2.575a1.5 1.5 0 0 0-2.121 0l-.3.3-1.089-.452V2A1.5 1.5 0 0 0 11 .5H9A1.5 1.5 0 0 0 7.5 2v.423l-1.09.452-.3-.3a1.5 1.5 0 0 0-2.121 0L2.576 3.99a1.5 1.5 0 0 0 0 2.121l.3.3L2.423 7.5H2A1.5 1.5 0 0 0 .5 9v2A1.5 1.5 0 0 0 2 12.5h.423l.452 1.09-.3.3a1.5 1.5 0 0 0 0 2.121l1.415 1.413a1.5 1.5 0 0 0 2.121 0l.3-.3 1.09.452V18A1.5 1.5 0 0 0 9 19.5h2a1.5 1.5 0 0 0 1.5-1.5v-.423l1.09-.452.3.3a1.5 1.5 0 0 0 2.121 0l1.415-1.414a1.5 1.5 0 0 0 0-2.121l-.3-.3.452-1.09H18a1.5 1.5 0 0 0 1.5-1.5V9A1.5 1.5 0 0 0 18 7.5Zm-8 6a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z"/>
+                                    </svg>
+                                <span class="sr-only">Settings</span>
+                            </button>
+                            <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 0">
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M18 2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2ZM2 18V7h6.7l.4-.409A4.309 4.309 0 0 1 15.753 7H18v11H2Z"/>
+                                        <path d="M8.139 10.411 5.289 13.3A1 1 0 0 0 5 14v2a1 1 0 0 0 1 1h2a1 1 0 0 0 .7-.288l2.886-2.851-3.447-3.45ZM14 8a2.463 2.463 0 0 0-3.484 0l-.971.983 3.468 3.468.987-.971A2.463 2.463 0 0 0 14 8Z"/>
+                                    </svg>
+                                <span class="sr-only">Timeline</span>
+                            </button>
+                            <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 0">
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z"/>
+                                        <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"/>
+                                    </svg>
+                                <span class="sr-only">Download</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-4 py-2 bg-white rounded-b-lg">
+                    <label for="editor" class="sr-only">Procesar</label>
+                    <textarea id="editor" rows="8" class="block w-full px-0 text-sm text-gray-800 bg-white border-0 focus:ring-0" placeholder="Escribe lo que sea..." required ></textarea>
+                </div>
+            </div>
+            <button type="submit" class="float-right px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                </svg>
+            </button>
+        </form>
+
+    </section>
+    <section class="mx-11 pb-10">
+        <div class="flex justify-center mb-5 clear-right">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="#1E52DC" class="size-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+            </svg>
+        </div>
+        <h1 class="text-3xl text-center font-bold mb-10">
+            ¿Como puedes usar Motor Acústico?
+        </h1>
+        <div className="grid lg:grid-cols-2 grid-cols-1">
+            <div class="relative">
+                <h2>Voces que se adaptan a todas tus ideas</h2>
+                <p>Genere voz de alta calidad en cualquier voz, estilo e idioma. Nuestro generador de voz de IA reproduce la entonación y las inflexiones humanas con una fidelidad excepcional, ajustando la entrega en función del contexto.</p>
+            </div>
+            <div class="relative">
+                <img src="assets/images/vibrant_sounds.webp" alt="Voces vibrantes">
+            </div>
+        </div>
+        <div className="grid lg:grid-cols-2 grid-cols-1">
+            <div class="relative">
+                <h2>Hacer que el contenido sea universalmente accesible</h2>
+                <p>
+                    Desde el texto a la voz hasta el doblaje con IA, nuestras herramientas cierran las brechas lingüísticas, restauran las voces de aquellos que las han perdido y hacen que las interacciones digitales se sientan más humanas, transformando la forma en que nos conectamos en línea.
+                </p>
+            </div>
+            <div class="relative">
+                <img src="assets/images/vibrant_sounds.webp" alt="Voces vibrantes">
+            </div>
+        </div>
+        <div className="grid lg:grid-cols-2 grid-cols-1">
+            <div class="relative">
+                <img src="assets/images/vibrant_sounds.webp" alt="Voces vibrantes">
+            </div>
+            <div class="relative">
+                <h2>Conjunto completo de herramientas de IA de voz</h2>
+                <p>
+                    Desde el texto a la voz hasta el doblaje con IA, nuestras herramientas cierran las brechas lingüísticas, restauran las voces de aquellos que las han perdido y hacen que las interacciones digitales se sientan más humanas, transformando la forma en que nos conectamos en línea.
+                </p>
+            </div>
+        </div>
+        <div className="grid lg:grid-cols-2 grid-cols-1">
+            <div class="relative">
+                <h2>La seguridad de la IA en Koe</h2>
+                <p>
+                    El audio con IA impulsa la creatividad, la productividad y la accesibilidad. Nuestro objetivo es crear productos seguros y fiables que impulsen la innovación y ayuden a superar las barreras de comunicación.
+                </p>
+            </div>
+            <div class="relative">
+                <img src="assets/images/vibrant_sounds.webp" alt="Voces vibrantes">
+            </div>
+        </div>
+    </section>
+    <section class="pb-10 bg-blue-700">
+        <div class="px-11 pt-10">
+            <h1 class="text-3xl text-left text-white font-bold mb-10">Testimonios</h1>
+        </div>
+    </section>
+    <section class="mt-10 pb-10 mx-11">
+        <div class="flex justify-center mb-2">
+            <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1E52DC" class="size-10">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+        </div>
+        <h1 class="text-3xl text-center font-bold mb-10">Precios</h1>
+    </section>
+    <section>
+        <!-- Llamado a la acción -->
+    </section>
+</body>
+</html>
